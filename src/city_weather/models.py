59f37e851fe64ae0d4a343e856015ed3b2c67717
Dmitry <minskiyda@gmail.com>
2024-05-28 21:44:01 +0100

    update models

--- src/city_weather/models.py
+++ src/city_weather/models.py
@@ -9,7 +9,6 @@ class City(models.Model):
     city_name = models.TextField(null=False, editable=False)
     country_code = models.TextField(null=True, editable=False)
 
-    city_raw = models.JSONField(editable=False)
     created_at = models.DateTimeField(null=True)
 
     def __str__(self) -> str:
@@ -33,26 +32,34 @@ class City(models.Model):
 
 class CityWeather(models.Model):
     id = models.AutoField(primary_key=True)
-    city = models.ForeignKey(
+    city = models.OneToOneField(
         "City",
         models.DO_NOTHING,
-        null=False,
-        blank=False,
-        editable=False,
     )
-    current_temperature_K = models.FloatField(null=False, editable=True)
-    current_condition = models.TextField(null=False, editable=True)
+    current_temperature_K = models.FloatField(null=True, blank=True, editable=True)
+    current_temperature_C = models.FloatField(null=True, blank=True, editable=True)
+    current_condition = models.TextField(null=True, blank=True, editable=True)
+    humidity = models.IntegerField(null=True, blank=True, editable=True)
+    wind_speed = models.FloatField(null=True, blank=True, editable=True)
 
-    weather_raw = models.JSONField(editable=True)
+    weather_raw = models.JSONField(null=True, blank=True, editable=True)
     created_at = models.DateTimeField(null=True)
     last_updated = models.DateTimeField(null=True)
 
     def save(self, *args, **kwargs):
         if not self.created_at:
             self.created_at = timezone.now()
-        if not self.last_updated:
-            self.last_updated = timezone.now()
+        self.last_updated = timezone.now()
+        if self.current_temperature_K:
+            self.current_temperature_C = self._kelvin_to_celcius()
+
         super().save(*args, **kwargs)
+
+    def _kelvin_to_celcius(self):
+        if not self.current_temperature_K:
+            return None
+        else:
+            return round(self.current_temperature_K - 273.15, 1)
 
     class Meta:
         managed = True
